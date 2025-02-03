513756a6067f036c7c1fe99b21226f9f7716442c
PaulTheSuper <104734324+PaulTheSuper@users.noreply.github.com>
2025-02-03 10:29:04 -0500

    new code
    yeah

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -6,29 +6,29 @@
 std::vector<int> randomNumbers(5);
 bool sorted = false;
 //--------------------------------------------------------------
-void ofApp::setup(){
- generateRandomNumbers();
+void ofApp::setup() {
+	generateRandomNumbers();
 }
 
 void ofApp::generateRandomNumbers() {
-	unsigned seed = 123;  
-	std::mt19937 gen(seed);  
-	std::uniform_int_distribution<> dis(10, 100); 
+	unsigned seed = 123;
+	std::mt19937 gen(seed);
+	std::uniform_int_distribution<> dis(10, 100);
 
 	randomNumbers.clear();
 	for (int i = 0; i < 5; ++i) {
-		randomNumbers.push_back(dis(gen));
+		randomNumbers.push_back(ofRandom(10, 101));
 	}
-	sorted = false;  
+	sorted = false;
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::update() {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::draw() {
 
 	ofDrawBitmapString("Press keys to control:", 20, 30);
 	ofDrawBitmapString("b - Bubble Sort", 20, 50);
@@ -50,15 +50,15 @@ void ofApp::draw(){
 		ofPoint(100, 500), ofPoint(200, 500), ofPoint(300, 500),
 		ofPoint(400, 500), ofPoint(500, 500)
 	};
-	
-	
+
+
 
 	for (int i = 0; i < 5; ++i) {
-		
+
 		if (i % 2 == 0)
-			ofSetColor(0, 0, 225); 
+			ofSetColor(0, 0, 225);
 		else
-			ofSetColor(225, 150, 0); 
+			ofSetColor(225, 150, 0);
 
 		ofDrawCircle(positions[i].x, positions[i].y, randomNumbers[i]);
 	}
@@ -76,11 +76,41 @@ void ofApp::bubbleSort()
 	sorted = true;
 }
 
-	
+
+
+int ofApp::partition(std::vector<int>& arr, int low, int high) {
+	int pivot = arr[high];
+	int i = low - 1;
+
+
+	for (int j = low; j < high; j++) {
+		if (arr[j] <= pivot) {
+			i++;
+			std::swap(arr[i], arr[j]);
+		}
+	}
+	std::swap(arr[i + 1], arr[high]);
+	return i + 1;
+}
+
+
+void ofApp::quickSort(std::vector<int>& arr, int low, int high)
+{
+	if (low < high) {
+
+		int pi = partition(arr, low, high);
+
+
+		quickSort(arr, low, pi - 1);
+		quickSort(arr, pi + 1, high);
+	}
+}
+
+
 
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+void ofApp::keyPressed(int key) {
 	if (key == 'b')
 	{
 		bubbleSort();
@@ -101,8 +131,18 @@ void ofApp::keyPressed(int key){
 	}
 	else if (key == 'r')
 	{
-		 generateRandomNumbers();
-		 draw();
+		///this is a new radnom
+		///test 2
+		randomNumbers.clear();
+		for (int i = 0; i < 5; i++) {
+			randomNumbers.push_back(ofRandom(10, 101));
+		}
+
+	}
+	else if (key == 'q')
+	{
+		quickSort(randomNumbers, 0, randomNumbers.size() - 1);
+		sorted = true;
 	}
 
 }
@@ -113,51 +153,51 @@ void ofApp::keyPressed(int key){
 
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
+void ofApp::keyReleased(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
+void ofApp::mouseMoved(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
+void ofApp::mouseDragged(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
+void ofApp::mousePressed(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
+void ofApp::mouseReleased(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
+void ofApp::mouseEntered(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
+void ofApp::mouseExited(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
+void ofApp::windowResized(int w, int h) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
+void ofApp::gotMessage(ofMessage msg) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::dragEvent(ofDragInfo dragInfo) {
 
 }
